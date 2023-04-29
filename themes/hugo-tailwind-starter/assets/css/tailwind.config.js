d9705cb62fdbd12622373f54714a0a1ee29fbfa7
Jose Garcia <jose.garcia.dev86@gmail.com>
2023-04-29 23:18:14 +0200

    Hugo-Tailwind Starter kit created

---
+++ themes/hugo-tailwind-starter/assets/css/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./content/**/*.{md,html}",
+        "./layouts/**/*.html",
+        "./themes/hugo-tailwind-starter/layouts/**/*.html",
+        ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
