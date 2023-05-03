a0cb88e5c5adc366966f0c3e691aea4225937952
Jose Garcia <jose.garcia.dev86@gmail.com>
2023-05-03 20:49:45 +0200

    Search bar Desktop and Mobile implemented

--- themes/hugo-tailwind-starter/assets/js/index.js
+++ themes/hugo-tailwind-starter/assets/js/index.js
@@ -2,10 +2,26 @@
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
-btn.addEventListener('click',navToggle);
+btn.addEventListener('click',function(){
+    toggleClasses(btn, menu, menu, 'open-menu', 'flex', 'hidden');
+}, false);
 
-function navToggle(){
-    btn.classList.toggle('open');
-    menu.classList.toggle('flex');
-    menu.classList.toggle('hidden');
+// Search Bar
+const searchBtn = document.getElementById('search-btn');
+const searchBar = document.getElementById('search-bar');
+
+searchBtn.addEventListener('click',function(){
+    toggleClasses( element2=searchBar, element3=searchBar, class2='block', class3='hidden');
+    }, false);
+
+
+
+// Utility functions
+// Function toggleClasses takes three elements and three classes and toggles those classes on each element. ELement 3 and Class 3 are optional.
+function toggleClasses(element1=null, element2, element3, class1=null, class2, class3) {
+    if (element1 && class1) {
+        element1.classList.toggle(class1);
+    }
+    element2.classList.toggle(class2);
+    element3.classList.toggle(class3);
 }
